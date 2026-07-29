7a2333d4da9b2e914c8e3abe3e260155680466e6
Ananya Pahwa <138243485+ananyapahwa@users.noreply.github.com>
2026-07-30 01:20:13 +0530

    Add: products-of-array-discluding-self - submission-0

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-0.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> pref(n), suff(n);
+        vector<int> ans(n);
+
+        //constructing prefix multiplication
+        pref[0] = nums[0];
+        for(int i=1; i<n; i++){
+            pref[i] = nums[i]*pref[i-1];
+        }
+
+        //constructing suffix multiplication
+        suff[n-1] = nums[n-1];
+        for(int i=n-2; i>=0; i--){
+            suff[i] = nums[i]*suff[i+1];
+        }
+
+        //calculating the answer
+        
+
+        ans[0] = suff[1];
+        ans[n-1] = pref[n-2];
+        for(int i=1; i<n-1; i++){
+            ans[i] = pref[i-1]*suff[i+1];
+        }
+
+        return ans;
+    }
+};
